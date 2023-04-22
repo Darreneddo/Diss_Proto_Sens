ad5d2630e82a02c50b547fbb22fad1c459111d04
Darreneddo <darrene94@gmail.com>
2023-04-22 17:52:01 +0100

    Sensor Reading Updated every 2 minutes while Monitor is powered on

--- ardRun.py
+++ ardRun.py
@@ -1,14 +1,17 @@
-#import 
+#import necessary libraries
 import serial
 import os
 
+#Initialise serial connection
 serial = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=5)
 
+#Main method which reads sensors and saves readings
 def main():
     readSensors()
     saveToTxt()
     #os.system("sudo poweroff")
 
+#Reads in each sensor values ad saves values to array
 def readSensors():
     try:
         readings = serial.readline(20).decode('utf-8').rstrip().split(',')
@@ -16,6 +19,7 @@ def readSensors():
 
         global pH, turb, temp
 
+        #Array values are then assigned to individual values
         pH = readings[0]
         turb = int(readings[1])
         temp = readings[2]
@@ -23,6 +27,7 @@ def readSensors():
     except:
         print("Error with reading sensors")
 
+#Saves readings to text files for Github Upload
 def saveToTxt():
     pHFile = open("pHFile.txt", "w+")
     pHFile.write(str(pH))
