37cd7fe444d14b24f72c5b5126bac5458f82a287
Darreneddo <darrene94@gmail.com>
2023-04-23 23:08:01 +0100

    Sensor Reading Updated every 2 minutes while Monitor is powered on

--- ardRun.py
+++ ardRun.py
@@ -23,17 +23,17 @@ def readSensors():
         pH = readings[0]
         turb = int(readings[1])
         temp = readings[2]
-        print("{8.89, 0.0, 7.63}")
+        print(readings)
     except:
         print("Error with reading sensors")
 
 #Saves readings to text files for Github Upload
 def saveToTxt():
     pHFile = open("pHFile.txt", "w+")
-    pHFile.write(str(8.89))
+    pHFile.write(str(pH))
     turbFile = open("turbFile.txt", "w+")
-    turbFile.write(str(0.0))
+    turbFile.write(str(turb))
     tempFile = open("tempFile.txt", "w+")
-    tempFile.write(str(7.63))
+    tempFile.write(str(temp))
 
 main()
